160189b11e332d018cc0cf3fd5b451cb1dec3465
akhilmunnuru26 <munnuruakhil26@gmail.com>
2026-04-05 08:47:18 +0530

    final submission code

--- src/index.ts
+++ src/index.ts
@@ -22,7 +22,7 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use(express.json()); // Parses incoming JSON payloads
+app.use(express.json()); 
 
 // Routes
 app.use('/api/tickets', ticketRoutes);
